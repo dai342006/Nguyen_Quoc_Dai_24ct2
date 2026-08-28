7509d09e2327074d1d93d6a159fb69c56bc07207
QuocDaiIT <dai542789@gmail.com>
2026-08-28 16:40:44 +0700

    Cap nhat giao dien SkillHub

--- 24CT2_Nguyen_Quoc_Dai/src/App.jsx
+++ 24CT2_Nguyen_Quoc_Dai/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import avatar from "./assets/avatar1.jpg";
 import "./App.css";
 
 const services = [
@@ -383,30 +384,55 @@ function Freelancer({ setPage }) {
   return (
     <main className="page">
       <div className="container">
+
         <div className="page-title">
           <p className="eyebrow">FREELANCER</p>
           <h1>Hồ sơ Freelancer</h1>
         </div>
 
         <div className="profile-layout">
+
           <section className="profile-card">
+
             <div className="profile-top">
-              <div className="profile-avatar">👨‍💻</div>
+
+              <div className="profile-avatar">
+                <img src={avatar} alt="Nguyễn Quốc Đại" />
+              </div>
+
               <div>
-                <h2>Nguyễn Minh Duy</h2>
+                <h2>Nguyễn Quốc Đại</h2>
                 <p>Lập trình viên & Web Developer</p>
-                <div className="rating-big">⭐ 4.9 <span>• Đà Nẵng</span></div>
+
+                <div className="rating-big">
+                  ⭐ 4.9 <span>• Đà Nẵng</span>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="profile-stats">
+              <div>
+                <strong>98%</strong>
+                <span>Hoàn thành</span>
+              </div>
+
+              <div>
+                <strong>120+</strong>
+                <span>Đơn hàng</span>
+              </div>
+
+              <div>
+                <strong>3 năm</strong>
+                <span>Kinh nghiệm</span>
               </div>
             </div>
-            <div className="profile-stats">
-              <div><strong>98%</strong><span>Hoàn thành</span></div>
-              <div><strong>120+</strong><span>Đơn hàng</span></div>
-              <div><strong>3 năm</strong><span>Kinh nghiệm</span></div>
-            </div>
+
             <p>
-              Xin chào! Mình chuyên xây dựng website bằng ReactJS, thiết kế giao
-              diện và tối ưu trải nghiệm người dùng.
+              Xin chào! Mình chuyên xây dựng website bằng ReactJS,
+              thiết kế giao diện và tối ưu trải nghiệm người dùng.
             </p>
+
             <div className="skills">
               {["ReactJS", "JavaScript", "HTML/CSS", "UI/UX"].map((skill) => (
                 <span key={skill}>{skill}</span>
